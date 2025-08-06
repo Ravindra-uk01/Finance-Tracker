c3c9b222581433369d64e10cb0b05572267c9f85
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-08-06 08:22:04 +0530

    bug fix 6

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install all dependencies
 npm install
+
+# Ensure prisma CLI has execute permission
+chmod +x ./node_modules/.bin/prisma
+
+# Generate Prisma client
 npx prisma generate
